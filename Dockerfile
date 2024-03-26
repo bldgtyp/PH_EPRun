f76a156cad3e116c09b8765111f1a09633261466
PH Tools <phtools@bldgtyp.com>
2024-03-26 15:47:45 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,27 @@
 # Use the existing EnergyPlus container as the base image
+# https://hub.docker.com/r/nrel/energyplus/tags
 FROM nrel/energyplus:23.2.0
 
 # Install any additional dependencies required for your Python scripts
 # For example, if you need Python and pip:
 RUN apt-get update && apt-get install -y python3 python3-pip
 
-# Set working directory
-WORKDIR /app
+# Install Python dependencies
+RUN pip3 install eppy
+RUN pip3 install python-dotenv
+RUN pip3 install fastapi
+
 
 # Copy your Python scripts into the container
-COPY main.py /app/
+COPY main.py /EnergyPlus-23.2.0-7636e6b3e9-Linux-Ubuntu20.04-x86_64/
+COPY WeatherData/ /EnergyPlus-23.2.0-7636e6b3e9-Linux-Ubuntu20.04-x86_64/WeatherData/
+COPY Exercise1A.idf /EnergyPlus-23.2.0-7636e6b3e9-Linux-Ubuntu20.04-x86_64/
+
+# Set working directory
+WORKDIR /EnergyPlus-23.2.0-7636e6b3e9-Linux-Ubuntu20.04-x86_64
+
+ENV ENERGYPLUS_INSTALL_DIR=/EnergyPlus-23.2.0-7636e6b3e9-Linux-Ubuntu20.04-x86_64
 
 # Set the entrypoint to your Python script
-CMD ["EnergyPlus", "--help"]
-# CMD ["python3", "main.py"]
+# CMD ["EnergyPlus", "--help"]
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,60 +1,48 @@
-print("testing")
+print("- " * 25)
+print("Starting main.py")
 
 import os
 import pathlib
 
-dir = pathlib.Path("EnergyPlus")
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError as e:
+    print(e)
+
+from eppy import modeleditor
+from eppy.modeleditor import IDF
 
 
 def list_files(start_path):
     for root, dirs, files in os.walk(start_path):
-        level = root.replace(start_path, "").count(os.sep)
+        level = root.replace(str(start_path), "").count(os.sep)
         indent = " " * 4 * (level)
         print("{}{}/".format(indent, os.path.basename(root)))
         sub_indent = " " * 4 * (level + 1)
         for file in files:
-            print("{}{}".format(sub_indent, file))
+            if ".idd" in str(file):
+                print("{}{}".format(sub_indent, file))
 
 
-if __name__ == "__main__":
-    start_path = "/"
-    print(f"Folder structure of {start_path}:")
-    list_files(start_path)
-
-
-# import os
-# import pathlib
-
-# try:
-#     from dotenv import load_dotenv
-
-#     load_dotenv()
-# except ImportError as e:
-#     print(e)
-
-# from eppy import modeleditor
-# from eppy.modeleditor import IDF
-
-# # -- Load environment variables from .env file
-
+print("os.getcwd():", os.getcwd())
 # # -- Setup the IDD and IDF Files needed
-# ep_install_dir = pathlib.Path(os.environ.get("ENERGYPLUS_INSTALL_DIR") or ".")
-# print(f"EnergyPlus Directory | {ep_install_dir.resolve()}")
+ep_install_dir = pathlib.Path(os.environ.get("ENERGYPLUS_INSTALL_DIR") or ".")
+print(
+    f"EnergyPlus Directory | {ep_install_dir.resolve()} [exists={ep_install_dir.exists()}]"
+)
 
-# idd_file = ep_install_dir / "Energy+.idd"
-# print(f"IDD File | {str(idd_file.resolve())} [exists={idd_file.exists()}]")
+idd_file = ep_install_dir / "Energy+.idd"
+print(f"IDD File | {str(idd_file.resolve())} [exists={idd_file.exists()}]")
 
-# idf_file = pathlib.Path("Exercise1A.idf")
-# print(f"IDF File | {str(idf_file.resolve())} [exists={idf_file.exists()}]")
+idf_file = pathlib.Path("Exercise1A.idf")
+print(f"IDF File | {str(idf_file.resolve())} [exists={idf_file.exists()}]")
 
-# epw_file = pathlib.Path("WeatherData/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw")
-# print(f"EPW File | {str(epw_file.resolve())} [exist={epw_file.exists()}]")
-
+epw_file = pathlib.Path("WeatherData/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw")
+print(f"EPW File | {str(epw_file.resolve())} [exist={epw_file.exists()}]")
 
 # -- Mess with the IDF file
-# IDF.setiddname(idd_file)
-# idf_model = IDF(idf_file, epw_file)
+IDF.setiddname(idd_file)
+idf_model = IDF(idf_file, epw_file)
 # idf_model.run()
-# print(idf_model.idfobjects["BUILDING"])
-# bldg = idf_model.idfobjects["BUILDING"][0]
-# print(bldg.Name)
